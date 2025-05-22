48e6b943d6e59ec484a4969c7baa75d608196566
Ana Cambraia <anabeatrizsilvacambraia@gmail.com>
2025-05-22 20:14:53 +0000

    feat: solução d tarefa

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,14 @@
-// Escreva seu código aqui!
+const meuNome : string = 'Ana Beatriz';
+let quantAlunos;
+const sala :{
+    nome : string;
+    cor : string;
+    altura : number;
+    energia : boolean;
+}  = {
+    nome : 'Laboratório de Informática',
+    cor : 'Branco',
+    altura : 3,
+    energia : true
+};
+console.log (meuNome, quantAlunos, sala.altura, sala.cor, sala.energia, sala.nome);
